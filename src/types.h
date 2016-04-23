338dd2d1318f44924b53bf90639e5bea020f8706
Cameron Whipple <whipple.cameron89@gmail.com>
2016-04-23 08:30:39 -0500

    Moved the return status codes to the portion of the header that is being utilized.

--- src/types.h
+++ src/types.h
@@ -84,4 +84,21 @@ typedef struct
   
 } LKM_Operation_t;
 
+/**
+ * @brief Represents possible return codes from functions within the module
+ *
+ * @details
+ *    Represents the possible return code values from the functions within the module.
+ * TODO -- Remove
+ */
+typedef enum
+{
+
+  OK               =  0, /**< Function was successful */
+  ERROR            = -1, /**< An unknown error occurred */
+  INVALID_PID      = -2, /**< Invalid PID was provided or the PID is not currently active */
+  INVALID_ARG      = -3, /**< Invalid argument provided to the function */
+  SIG_INSTALL_FAIL = -4, /**< Installation of a singal handler failed */
+} Status;
+
 #endif
